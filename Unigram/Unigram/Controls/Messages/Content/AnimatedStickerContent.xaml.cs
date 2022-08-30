7b2d7e3fae7a262d3d5131c2a1a19513e86967d1
Dezane Woods <62675906+dezanewoods3@users.noreply.github.com>
2022-08-29 23:30:04 -0500

    Initialize vnext

---
+++ Unigram/Unigram/Controls/Messages/Content/AnimatedStickerContent.xaml.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Telegram.Td.Api;
+using Unigram.Common;
+using Unigram.Services;
+using Unigram.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Unigram.Controls.Messages.Content
+{
+    public sealed partial class AnimatedStickerContent : HyperlinkButton, IContentWithFile
+    {
+        private MessageViewModel _message;
+        public MessageViewModel Message => _message;
+
+        public AnimatedStickerContent(MessageViewModel message)
+        {
+            InitializeComponent();
+            UpdateMessage(message);
+        }
+
+        public void UpdateMessage(MessageViewModel message)
+        {
+            _message = message;
+
+            var sticker = GetContent(message);
+            if (sticker == null)
+            {
+                return;
+            }
+
+            if (message.Content is MessageText text)
+            {
+                Width = Player.Width = 200 * message.ProtoService.Config.GetNamedNumber("emojies_animated_zoom", 0.625);
+                Height = Player.Height = 200 * message.ProtoService.Config.GetNamedNumber("emojies_animated_zoom", 0.625);
+                Player.ColorReplacements = Emoji.GetColorReplacements(text.Text.Text);
+            }
+            else if (message.Content is MessageDice)
+            {
+                Width = Player.Width = 200 * message.ProtoService.Config.GetNamedNumber("emojies_animated_zoom", 0.625);
+                Height = Player.Height = 200 * message.ProtoService.Config.GetNamedNumber("emojies_animated_zoom", 0.625);
+                Player.ColorReplacements = null;
+            }
+            else
+            {
+                Width = Player.Width = 200;
+                Height = Player.Height = 200;
+                Player.ColorReplacements = null;
+            }
+
+            if (sticker.Thumbnail != null && !sticker.StickerValue.Local.IsDownloadingCompleted)
+            {
+                UpdateThumbnail(message, sticker.Thumbnail.Photo);
+            }
+
+            UpdateFile(message, sticker.StickerValue);
+        }
+
+        public void UpdateMessageContentOpened(MessageViewModel message) { }
+
+        public void UpdateFile(MessageViewModel message, File file)
+        {
+            var sticker = GetContent(message);
+            if (sticker == null)
+            {
+                return;
+            }
+
+            if (sticker.Thumbnail != null && sticker.Thumbnail.Photo.Id == file.Id)
+            {
+                UpdateThumbnail(message, file);
+                return;
+            }
+            else if (sticker.StickerValue.Id != file.Id)
+            {
+                return;
+            }
+
+            if (file.Local.IsDownloadingCompleted)
+            {
+                if (SettingsService.Current.Diagnostics.PlayStickers)
+                {
+                    LayoutRoot.Background = null;
+
+                    Player.IsCachingEnabled = SettingsService.Current.Diagnostics.CacheStickers;
+                    Player.IsLoopingEnabled = (message.Content is MessageDice dice && dice.Value == 0) || (message.Content is MessageSticker && SettingsService.Current.Stickers.IsLoopingEnabled);
+                    Player.Source = new Uri("file:///" + file.Local.Path);
+                }
+                else
+                {
+                    Player.Source = null;
+                }
+            }
+            else if (file.Local.CanBeDownloaded && !file.Local.IsDownloadingActive)
+            {
+                message.ProtoService.DownloadFile(file.Id, 1);
+            }
+        }
+
+        private void UpdateThumbnail(MessageViewModel message, File file)
+        {
+            if (file.Local.IsDownloadingCompleted)
+            {
+                LayoutRoot.Background = new ImageBrush { ImageSource = PlaceholderHelper.GetWebPFrame(file.Local.Path) };
+            }
+            else if (file.Local.CanBeDownloaded && !file.Local.IsDownloadingActive)
+            {
+                message.ProtoService.DownloadFile(file.Id, 1);
+            }
+        }
+
+        public bool IsValid(MessageContent content, bool primary)
+        {
+            // We can't recycle it as we must destroy CanvasAnimatedControl on Unload.
+            return false;
+
+            if (content is MessageSticker sticker)
+            {
+                return sticker.Sticker.IsAnimated;
+            }
+            else if (content is MessageText text && text.WebPage != null && !primary)
+            {
+                return text.WebPage.Sticker != null && text.WebPage.Sticker.IsAnimated;
+            }
+
+            return false;
+        }
+
+        private Sticker GetContent(MessageViewModel message)
+        {
+            var content = message.GeneratedContent ?? message.Content;
+            if (content is MessageSticker sticker)
+            {
+                return sticker.Sticker;
+            }
+            else if (content is MessageText text && text.WebPage != null)
+            {
+                return text.WebPage.Sticker;
+            }
+
+            return null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var sticker = GetContent(_message);
+            if (sticker == null)
+            {
+                return;
+            }
+
+            if (_message.Content is MessageDice)
+            {
+                Window.Current.ShowTeachingTip(this, Strings.Resources.DiceInfo, _message.IsOutgoing && !_message.IsChannelPost ? TeachingTipPlacementMode.TopLeft : TeachingTipPlacementMode.TopRight);
+            }
+            else if (_message.Content is MessageText)
+            {
+                Player.Play();
+            }
+            else
+            {
+                _message.Delegate.OpenSticker(sticker);
+            }
+        }
+    }
+}
